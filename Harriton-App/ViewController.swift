e6d9aa7d5a38431c4a7a6ce615ce5951257d0b4c
Nolan Gelinas <Nolangelinas@gmail.com>
2018-10-24 11:02:58 -0400

    Initial Commit

---
+++ Harriton-App/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Harriton-App
+//
+//  Created by Nolan Gelinas (student HH) on 10/24/18.
+//  Copyright © 2018 Nolan Gelinas. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
